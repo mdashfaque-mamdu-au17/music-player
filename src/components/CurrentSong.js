2ef9ad62927ed1060b01b51981dda34ddb703e6f
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-09-14 12:17:06 +0530

    overflowing in current song is singer name and song name is fixed

--- src/components/CurrentSong.js
+++ src/components/CurrentSong.js
@@ -2,6 +2,7 @@ import { FiHeart } from "react-icons/fi";
 import React from "react";
 import Default from "../assets/default-image.jpeg";
 const CurrentSong = ({ imageSrc, songName, singerName }) => {
+	console.log(singerName.length)
 	return (
 		<div className="flex items-center gap-4">
 			<div className="w-10 h-10 lg:w-[74px] lg:h-[74px]">
@@ -13,9 +14,9 @@ const CurrentSong = ({ imageSrc, songName, singerName }) => {
 			</div>
 			<div className="flex flex-col">
 				<h5 className="text-base font-medium text-white text-ellipsis">
-					{songName}
+					{songName.length <= 16 ? songName : songName.substring(0, 12)+'...'}
 				</h5>
-				<p className="text-sm text-theme-gray-700 text-clip">{singerName}</p>
+				<p className="text-sm text-theme-gray-700 text-clip">{singerName.length <= 17 ? singerName : singerName.substring(0, 13)+'...'}</p>
 			</div>
 			<button className="text-theme-gray-700 hover:text-white">
 				<FiHeart size={20} />
